81dc78ec7e965c69aa4eaf977c24340a532d2147
Nexius <natepod123@gmail.com>
2019-02-10 17:01:24 -0600

    Add files via upload
    qword.sh automates the installation process

---
+++ qword.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Clone repo wherever you like
+git clone https://github.com/qword-os/qword.git
+cd qword
+# Build and install echfs-utils (used to build the root fs image)
+cd host/echfs-utils
+make
+# This will install echfs-utils in /usr/local
+sudo make install
+# Else specify a PREFIX variable if you want to install it elsewhere
+#make PREFIX=<myprefix> install
+# Now build the toolchain (this step will take a while)
+cd ../toolchain
+# You can replace the 4 in -j4 with your number of cores + 1
+./make_toolchain.sh -j4
+# Go back to the root of the tree
+cd ../..
+# Build the ports distribution
+cd root/src
+./makeworld.sh -j4
+./makeworld.sh clean
+# Now to build qword itself
+cd ../..
+# You might need to use gmake instead of make here on FreeBSD
+make clean && make DEBUG=qemu img && sync
